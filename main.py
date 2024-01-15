6ee5fd84dd311e1bf3939fdde92ca9a5e5e9ea1a
Joe <josephesposito0424@gmail.com>
2024-01-15 15:57:08 -0500

    /current-movies path updated

--- main.py
+++ main.py
@@ -3,32 +3,30 @@ import requests
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
-url="https://www.imdb.com/showtimes"
+#url="https://www.imdb.com/showtimes"
+url = "https://www.imdb.com/showtimes/location"
 
 def scrape_movies(url):
     response = requests.get(url)
     if response.status_code != 200:
-        print("Error: Status Code " + response.status_code)
+        print("Error fetching results, status code: " + response.status_code)
         return
 
     soup = BeautifulSoup(response.text, 'html.parser')
-    list_item = soup.find_all('div', {'class': 'list_item'})
-    titles = []
-
-    for item in list_item:
-        #Goes to the child of the <span> tag containing the <a> link with the value being the movie name
-        title = item.find('span', {"itemprop": True}).find('a', {"title": True})
+    movie_list = soup.find_all('div', {'class': 'lister-item mode-grid'})
+    movie_titles = []
+    for movie in movie_list:
+        title = movie.find('div', {'class': 'title'}).find('a', {'href': True})
         if title is not None:
-            titles.append(title.text)
-            #print(title.text)
-    print(titles)
-    return
+            movie_titles.append(title.text)
+            print(title.text)
 
-@app.route("/<region>/<postal_code>", methods=['GET'])
-def movieData(region, postal_code):
+    return movie_titles
+
+@app.route("/current-movies/<region>/<postal_code>", methods=['GET'])
+def currently_playing(region, postal_code):
     print(url + "/" + region + "/" + postal_code)
-    scrape_movies(url + "/" + region + "/" + postal_code)
-    return jsonify(message="Getting movies for " + postal_code + ", " + region)
+    return jsonify(scrape_movies(url + "/" + region + "/" + postal_code))
 
 if __name__ == "__main__":
     app.run(port=7777)
